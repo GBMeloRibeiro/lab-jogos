66c9555703e8ba790037480b8aa14ed8dbebee56
Gabriel Melo <160269582+GBMeloRibeiro@users.noreply.github.com>
2024-10-11 17:07:48 -0300

    Atualizei o pong, dia 2, 11/10

--- pong/main.py
+++ pong/main.py
@@ -1,17 +1,26 @@
 from PPlay.window import *
 from PPlay.gameimage import *
-janela = Window(1053, 629)
+from PPlay.gameobject import *
+janela = Window(1051, 626)
 cenario = GameImage("background.jpg")
 bola = GameImage("ball.png")
+jogador = GameImage("barra.jpg")
+computador = GameImage("barra.jpg")
 bola.x = janela.width/2 - bola.width/2
 bola.y = janela.height/2 - bola.height/2
+jogador.x = 1031
+jogador.y = janela.height/2
+computador.x = 5
+computador.y = janela.height/2
 vel = 6
 while True:
     bola.x += vel
-    if bola.x >= 1053-bola.width:
-        vel += -12
+    if bola.x >= 1051-bola.width:
+        vel = vel*-1.2
     if bola.x <= 0:
-        vel += +12
+        vel = vel*-1.2
     cenario.draw()
     bola.draw()
+    jogador.draw()
+    computador.draw()
     janela.update()
